467148020706f8c344ba23cf02bed90731287b37
马冷静 <malengjing@malengjingdeMacBook-Air.local>
2026-05-20 17:35:25 +0800

    fix: 收盘后也展示数据+今日概览可点击交互

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'stock-radar-v8';
+const CACHE = 'stock-radar-v9';
 const ASSETS = ['./stock-monitor.html', './manifest.json', './icon-192.svg', './icon-512.svg'];
 
 self.addEventListener('install', e => {
